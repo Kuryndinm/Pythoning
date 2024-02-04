2e6afadb5c8437d4c532422c54078b64c0a38826
Михаил Курындин <kuryndinm@gmail.com>
2024-02-04 18:51:32 +0500

    Just upgrading litting

--- guess number/guess_number.py
+++ guess number/guess_number.py
@@ -1,22 +1,19 @@
 from random import randint
 
-guess_number = randint(1,100)
+guess_number = randint(1, 100)
 check = False
 
-def guess_number_game(guess_number, user_number, check):
-     
+
+def guess_number_game(guess_number, user_number):
+
     if guess_number > user_number:
         return 'Ваше число меньше того, что загадано'
     elif guess_number < user_number:
         return 'Ваше число больше того, что загадано'
     else:
-        check = True
         return 'Отличная интуиция! Вы угадали число :).'
 
 
-
-  # Прогон через цикл while
-
+# Прогон через цикл while
 while not check:
-    print(guess_number_game(guess_number, int(input()), check)) 
-
+    print(guess_number_game(guess_number, int(input())))
